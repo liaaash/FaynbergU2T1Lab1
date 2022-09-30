4113a5d236899285c8ead4f7c8b903845b198bad
student <bonniec15@nycstudents.net>
2022-09-30 12:03:36 -0400

    yep

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class Point {
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int X, int Y) {
+        x = X;
+        y = Y;
+    }
+
+    public Point(int z) {
+        x = z;
+        y = z;
+    }
+
+    public Point() {
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int newX) {
+        x = newX;
+    }
+
+    public void setY(int newY) {
+        y = newY;
+    }
+
+    public String coordinate() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public String quadrant() {
+        if (x > 0 && y > 0) {
+            return "I";
+        }
+        if (x < 0 && y > 0) {
+            return "II";
+        }
+        if (x < 0 && y < 0) {
+            return "III";
+        }
+        if (x > 0 && y < 0) {
+            return "IV";
+        }
+        if (x == 0 && y == 0) {
+            return "origin";
+        }
+        if ((x == 0 && y != 0) || (x != 0 && y == 0)) {
+            return "on an axis";
+        }
+        return null;
+    }
 }
